3f94ab59cf3e908f301c84e5fee6e08a85bf2da6
FilipeLaceerda <filipeelacerda@gmail.com>
2021-08-07 16:31:05 -0300

    json

--- src/pages/feed/index.js
+++ src/pages/feed/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text } from 'react-native';
 
 
 function Feed(){
+const FEEDS_POR_PAGINA = 4;
+
+
     return(
         <Text>Feed BestAuto!</Text>
     );
