b41d0fdb6ec39257821f7beb8e7822290d21be7a
FilipeLaceerda <filipeelacerda@gmail.com>
2021-08-11 18:14:52 -0300

    commit

--- src/screens/Agendamento/index.js
+++ src/screens/Agendamento/index.js
@@ -5,15 +5,16 @@ import DatePicker from 'react-native-datepicker';
 export function Agendamento() {
   const [data, setData] = useState('');
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{flex: 1,justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 18}}>Em qual data deseja marcar o serviço?</Text>
       <DatePicker
-        style={{width: 200}}
+        style={{width: 300}}
         date={data}
         format="DD-MM-YYYY"
         maxDate="31-12-9999"
         onDateChange={setData}
       />
-      <Text style={{fontSize: 18}}>O horário está marcado para {data}</Text>
+      <Text style={{fontSize: 18, marginTop: 250}}>O serviço está marcado para {data}</Text>
     </View>
   );
 }
